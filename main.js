3d6be2e035e740b2608322d0f6793712d2d34c5b
Matt Williamson <mathewbwilliamson@gmail.com>
2018-08-20 14:41:28 -0400

    added function 3

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 'use strict';
 
 function createGreeting() {
-    console.log('Hi, my name is Matt and I am 36 years old.');
+    return 'Hi, my name is Matt and I am 36 years old.';
 }
 
-createGreeting();
+const greeting = createGreeting();
+
+console.log(greeting);
