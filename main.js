4f150e9d7339468db47a1d91d27b1eef95f76f4b
Matt Williamson <mathewbwilliamson@gmail.com>
2018-08-20 14:46:10 -0400

    added function 5

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 'use strict';
 
 function createGreeting(name, age) {
-    return `Hi, my name is ${name} and I am ${age} years old.`;
+    const yearOfBirth = 2018 - age;
+    return `Hi, my name is ${name} and I am ${age} years old. I was born in ${yearOfBirth}.`;
 }
 
 const greeting = createGreeting('Matt', 37);
